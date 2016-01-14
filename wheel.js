f3de754c49d681aa3c9edd42a008ab65e2c7133f
Ali Kashani <1kashaniali@gmail.com>
2016-01-13 17:47:51 -0800

    orange

--- wheel.js
+++ wheel.js
@@ -9,3 +9,21 @@
 var order = ['orange chicken', 'mooshu', 'steamed rice', 'fresh spring rolls'];
 
 console.log(order);
+
+function addTwo(add2, add3) {
+      function add2(uno, dos) {
+            return uno + dos;
+      }
+
+
+
+      function add3(uno, dos, tres) {
+            return uno + dos + tres;
+      }
+
+      return add3(1, 2, 3) + add2(5, 10);
+
+
+}
+
+console.log(addTwo());
