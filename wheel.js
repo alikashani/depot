8cc05049e493d9dde874ce553320f2aaffff0c99
Eela Rabbani <lettittraiin@eelarabsmacbook.att.net>
2016-01-13 17:35:09 -0800

    test

---
+++ wheel.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * WHEEL OF LIFE
+ * -- Ali Kashani --
+ * >>>> 2016 <<<<
+ *
+ */
+
+var order = ['orange chicken', 'mooshu', 'steamed rice', 'fresh spring rolls'];
+
+console.log(order);
