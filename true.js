57f5aed0cd1d53beb42523061e87dd37fd1420c0
alikashani <1kashaniali@gmail.com>
2016-02-20 03:04:12 -0800

    prac

--- true.js
+++ true.js
@@ -1,6 +1,22 @@
+const fs = require('fs');
+
+var output = fs.readFileSync('data.txt', 'utf8')
+	.trim()
+	.split('\r\n')
+	.map( line => line.split('\t'))
+	.reduce( (customers, line) => {
+		customers[line[0]] = customers[line[0]] || []
+		customers[line[0]].push({
+			name: line[1],
+			price: line[2],
+			quantity: line[3]
+		})
+		return customers
+	}, {})
+
+console.log((JSON.stringify(output, null, 4)).trim())
+
 var fun = true ? 1 : 0
 
-
-const orange = () => fun ? 'lemons' : 'limes'
-
+const orange = () => fun ? '\nlemons' : '\nlimes'
 console.log(orange())
