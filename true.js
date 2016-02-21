3c0097644a3c94e7c08a7d30a3a437000e160af3
Ali Kashani <1kashaniali@gmail.com>
2016-02-21 03:06:20 -0800

    fin

--- true.js
+++ true.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 var output = fs.readFileSync('data.txt', 'utf8')
 	.trim()
 	.split('\r\n')
-	.map( line => line.split('\t'))
+	.map( line => line.split('\t') )
 	.reduce( (customers, line) => {
 		customers[line[0]] = customers[line[0]] || []
 		customers[line[0]].push({
@@ -20,3 +20,6 @@ var fun = true ? 1 : 0
 
 const orange = () => fun ? '\nlemons' : '\nlimes'
 console.log(orange())
+
+var price = 8123;
+console.log( ( /* 5 + 3 */ -9 + 6/*8*/ / 2 ) / /*+4*/ -10 )
