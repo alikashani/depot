0e445695a3933fa3b9ed4226a643040f0871b927
Ali Kashani <1kashaniali@gmail.com>
2016-02-23 23:31:14 -0800

    data-structs & algo

---
+++ js/data-struct/vertex.js
@@ -0,0 +1,15 @@
+(function (exports) {
+      'use strict';
+
+      /**
+       * >>> Cloned from mgechev <<<
+       * >>> for personal studying purposes <<<
+       *
+       * Graph vertex
+       * @constructor
+       * @public
+       * @param {Number} id Id of the vertex
+       * @module data-structures/vertex
+       */
+      exports.Vertex = (id) => { this.id = id };
+})(typeof window === 'undefined' ? module.exports : window);
