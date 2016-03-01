5ef0154a302bae3cd55dd55515e82e1e3b4b9ecd
alikashani <1kashaniali@gmail.com>
2016-02-29 22:05:36 -0800

    bSearch

---
+++ js/algo/data-struct/vertex.js
@@ -0,0 +1,14 @@
+(function (exports) {
+      'use strict';
+      /**
+       * >>> Cloned from mgechev <<<
+       * >>> for personal studying purposes <<<
+       *
+       * Graph vertex
+       * @constructor
+       * @public
+       * @param {Number} id Id of the vertex
+       * @module data-structures/vertex
+       */
+      exports.Vertex = (id) => { this.id = id };
+})(typeof window === 'undefined' ? module.exports : window);
