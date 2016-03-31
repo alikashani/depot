ffeef2c5e81e841fb36cb5432d867a3e98cc5e57
alikashani <1kashaniali@gmail.com>
2016-03-31 13:27:21 -0700

    sorting

--- js/misc/packages.js
+++ js/misc/packages.js
@@ -1,29 +1,45 @@
-var binderFullOfNames = [
-	{ name: "Sean" },
-	{ name: "John" },
-	{ name: "Mark" },
-	{ name: "Bob" },
-];
+(function() {
+	'use strict';
 
-var fruit = ['tomato', 'orange', 'apple', 'cherry'];
+	var binderFullOfNames = [
+		{ name: "Sean" },
+		{ name: "John" },
+		{ name: "Mark" },
+		{ name: "Bob" },
+	];
 
-console.log(fruit.sort());
+	var fruit = ['tomato', 'orange', 'apple', 'cherry'];
 
-var rolodex = binderFullOfNames.sort((a, b) => {
-	if (a.name > b.name) return 1;
-	if (a.name > b.name) return -1;
-	return 0;
-});
 
-console.log(binderFullOfNames);
+	var rolodex = binderFullOfNames.sort(function (a, b) {
+		if (a.name > b.name) return 1;
+		if (a.name > b.name) return -1;
+		return 0;
+	});
 
-function fn(a){
-	if (typeof(a) != 'number') console.log('invalid type');
-	return a + 2;
-}
 
-var ali = 20;
+	function fn(a){
+		if (typeof(a) != 'number') {
+			console.log('invalid parameter passed');
+			return;
+		}
+		return a + 2;
+	}
 
-var nAli = fn(ali);
+	var ali = 1024;
 
-console.log(nAli);
+	var nAli = fn(ali);
+
+
+
+	fruit.sort().forEach(console.log);
+
+	function forEachTwo(array, action) {
+		for (var i = 0; i < array.length; i++)
+			action(array[i]);
+	}
+
+	forEachTwo(fruit, console.log);
+
+
+})();
