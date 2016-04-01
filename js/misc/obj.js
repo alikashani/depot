a726473b4d7e7862e2dd9db259941f3f20aecd18
alikashani <1kashaniali@gmail.com>
2016-04-01 13:25:13 -0700

    lint

--- js/misc/obj.js
+++ js/misc/obj.js
@@ -14,64 +14,67 @@
 //       configurable: true
 // });
 
-'use strict'
+(function() {
+	'use strict';
+	
+	var config = {
+		writable: true,
+		enumerable: true,
+		configurable: true
+	};
 
-var config = {
-      writable: true,
-      enumerable: true,
-      configurable: true
-};
-
-var defineProperty = function(obj, name, value) {
-      config.value = value;
-      Object.defineProperty(obj, name, config);
-};
-
-
-var fromPrototype = function(prototype, object) {
-      var newObject = Object.create(prototype);
-
-      for (var prop in object) {
-            if (object.hasOwnProperty(prop)) {
-                  newObject[prop] = object[prop];
-            }
-      }
-
-      return newObject;
-};
-
-var person = {
-      toString: function() {
-            return this.firstName + ' ' + this.lastName;
-      }
-};
-
-var man = fromPrototype(person, { sex: 'male' });
-
-var ali = fromPrototype(man, {
-      firstName: 'Ali',
-      lastName: 'Kashani'
-});
-
-// console.log(ali);
-// console.log(ali.sex);
-// console.log(ali.toString());
-
-var liver = fromPrototype(ali, {
-      condition: 'healthy'
-});
+	var defineProperty = function(obj, name, value) {
+		config.value = value;
+		Object.defineProperty(obj, name, config);
+	};
 
 
-// console.log(liver);
+	var fromPrototype = function(prototype, object) {
+		var newObject = Object.create(prototype);
 
-const makeFruit = (taste, amount, color) => ({ taste, amount, color })
+		for (var prop in object) {
+		    if (object.hasOwnProperty(prop)) {
+		        newObject[prop] = object[prop];
+		    }
+		}
 
-console.log(liver);
+		return newObject;
+	};
 
-var food = {
-      junk: ['pizza', 'cheeseburger', 'chips', 'candy', 'ice cream'],
-      herbs: ['cilantro', 'parsley', 'basil', 'dill'],
-      bread: ['sourdough', 'bagette', 'whole wheat', 'white']
-}
+	var person = {
+		toString: function() {
+			return this.firstName + ' ' + this.lastName;
+		}
+	};
 
-console.log(food.junk);
+	var man = fromPrototype(person, { sex: 'male' });
+
+	var ali = fromPrototype(man, {
+		firstName: 'Ali',
+		lastName: 'Kashani'
+	});
+
+	// console.log(ali);
+	// console.log(ali.sex);
+	// console.log(ali.toString());
+
+	var liver = fromPrototype(ali, {
+	    condition: 'healthy'
+	});
+
+
+	// console.log(liver);
+
+	const makeFruit = (taste, amount, color) => ({ taste, amount, color });
+
+	console.log(liver);
+
+	var food = {
+		junk: ['pizza', 'cheeseburger', 'chips', 'candy', 'ice cream'],
+		herbs: ['cilantro', 'parsley', 'basil', 'dill'],
+		bread: ['sourdough', 'bagette', 'whole wheat', 'white']
+	};
+
+	console.log(food.junk);
+
+})();
