759ed3be1709bdb0545b67ca115eea2d9d6e722a
alikashani <1kashaniali@gmail.com>
2016-04-11 16:37:49 -0700

    cleaned mess

--- js/ee/cart.js
+++ js/ee/cart.js
@@ -6,3 +6,26 @@ const addToCart = (cart, item, quantity) => {
 	});
 	return cart;
 };
+
+test('addToCart()', assert => {
+	const msg = 'addToCart() should add a new item to the cart.';
+	const originalCart = {
+		items: []
+	};
+	const cart = addToCart(
+		originalCart,
+		{
+			name: "Digital SLR Camera",
+			price: '1495'
+		},
+		1
+	);
+
+	const expected = 1; // num items in cart
+	const actual = cart.items.length;
+
+	assert.equal(actual, expected, msg);
+
+	assert.deepEqual(originalCart, cart, 'mutates original cart.');
+	assert.end();
+});
