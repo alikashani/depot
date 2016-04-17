1aa32ba9efcbb15a54d85f3589472278a8b01a1c
alikashani <1kashaniali@gmail.com>
2016-04-17 14:17:56 -0700

    prog

--- js/ejs/EJS01.js
+++ js/ejs/EJS01.js
@@ -11,26 +11,26 @@
 // 	else
 // 		console.log(j);
 
-var mack = [];
-mack.push('Mack');
-mack.push('the', 'Knife');
-console.log(mack);
+var mack = []
+mack.push('Mack')
+mack.push('the', 'Knife')
+console.log(mack)
 
-mack.join(" ");
-console.log(mack);
+mack.join(" ")
+console.log(mack)
 
-mack.pop();
-console.log(mack);
+mack.pop()
+console.log(mack)
 
 const day1 = {
 	squirrel: false,
 	events: ['work', 'touched tree', 'pizza', 'running', 'television'],
 	orange: (arr) => { return arr.join(" ") }
-};
+}
 
-console.log(day1.squirrel);
-console.log(day1.orange(day1.events));
-console.log(day1.events);
+console.log(day1.squirrel)
+console.log(day1.orange(day1.events))
+console.log(day1.events)
 
 var desc = {
 	work: 'Went to work',
