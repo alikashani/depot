943073cc6e1d9ff6d064b371168ecc5f773d8f1b
alikashani <1kashaniali@gmail.com>
2016-05-31 21:15:49 -0700

    recursive iteration

--- js/misc/gen.js
+++ js/misc/gen.js
@@ -6,23 +6,21 @@ const fetch = require('node-fetch');
 //   .then( x => console.log('Title:', x))
 
 run(function *() {
-
   const uri = 'http://jsonplaceholder.typicode.com/posts/1';
   const response = yield fetch(uri);
-
   const post = yield response.json();
   const title = post.title;
-  console.log('Title:', title)
-
-});
+  return title
+})
+  .catch( error => console.error(error.stack))
+  .then( x => console.log('Run resulted in', x))
 
 function run(generator) {
   const iterator = generator()
-  const iteration = iterator.next()
-  const promise = iteration.value
-  promise.then( x =>  {
-    const anotherIterator = iterator.next(x)
-    const anotherPromise = anotherIterator.value
-    anotherPromise.then( y => iterator.next(y))
-  })
+  function iterate(iteration) {
+    if (iteration.done) return iteration.value
+    const promise = iteration.value
+    return promise.then( x => iterate(iterator.next(x)))
+  }
+  return iterate(iterator.next())
 }
