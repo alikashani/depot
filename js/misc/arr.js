ce51184f700437cfa1b91604621f108e8cc0b572
alikashani <1kashaniali@gmail.com>
2016-06-15 12:25:52 -0700

    new zip

--- js/misc/arr.js
+++ js/misc/arr.js
@@ -2,8 +2,7 @@ const arr = [ 4, 6, 1, 3, 6, 2, 6, 4, 4, 7, 3, 3];
 
 const rev = [];
 
-for (i = arr.length-1; i >= 0; i--) {
+for (i = arr.length-1; i >= 0; i--)
   rev.push(arr[i]);
-}
 
 console.log(rev);
