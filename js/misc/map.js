fa27632b5238ce5b2815ac7684db07957ca42fc2
alikashani <1kashaniali@gmail.com>
2016-07-10 00:32:57 -0700

    mapping

--- js/misc/map.js
+++ js/misc/map.js
@@ -5,19 +5,20 @@
 const fruit = ['apple', 'strawberry', 'orange', 'tomato', 'plum', 'peach', 'kiwi'];
 
 const veg = [
-  { name : 'cucumber', color: 'green' },
-  { name : 'tomato', color: 'green' },
-  { name : 'onion', color: 'green' },
-  { name : 'cilantro', color: 'green' },
-  { name : 'celery', color: 'green' },
-  { name : 'bell pepper', color: 'green' },
-]
+  { name : 'cucumber', color: 'green'  },
+  { name : 'tomato',   color: 'red'    },
+  { name : 'onion',    color: 'white'  },
+  { name : 'cilantro', color: 'green'  },
+  { name : 'celery',   color: 'green'  },
+  { name : 'carrot',   color: 'orange' }
+];
 
 let fruitLength = fruit.map(fruit => fruit.length);
 
 let selectFruit = fruit.filter(fruit => fruit.length > 5);
 
-let reducedFruit =
+let vegFilter = veg.filter(veggie => veggie.name.indexOf('c') === 0);
 
 console.log(fruitLength);
 console.log(selectFruit);
+console.log(vegFilter);
