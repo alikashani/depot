656d65c6df4f477b4a200cff18308c49861e0e96
alikashani <1kashaniali@gmail.com>
2016-07-14 17:37:16 -0700

    keygen challenge

---
+++ py/bward/boom.py
@@ -0,0 +1,12 @@
+'''
+    Python application that will process input string (250 character max)
+    Returns a JSON response in accordance with schema in data.json
+
+    Requirements:
+        - Generate a public/private RSA, DSA, or EC keypair
+          and persisting the keypair on the filesystem
+
+        - Subsequent invocations of application should read from the same file
+
+        - Document code: must at least annotate functions
+'''
