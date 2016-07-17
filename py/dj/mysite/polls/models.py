6e65819c971f0805eb0b3584ed3b9f363d0423fb
Ali Kashani <1kashaniali@gmail.com>
2016-07-17 10:54:40 -0700

    update

--- py/dj/mysite/polls/models.py
+++ py/dj/mysite/polls/models.py
@@ -12,3 +12,18 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.full_name
+
+class Article(models.Model):
+    pub_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.headline
