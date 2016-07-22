2074059710623d23906227a208571aa25dd7da0d
Ali Kashani <1kashaniali@gmail.com>
2016-07-22 00:58:07 -0700

    hero name changes

--- ng/angular2-tour-of-heroes/app/app.component.js
+++ ng/angular2-tour-of-heroes/app/app.component.js
@@ -11,16 +11,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var hero_detail_component_1 = require('./hero-detail.component');
 var HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
+    { id: 11, name: 'Mr. Mean' },
+    { id: 12, name: 'Parco' },
     { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
+    { id: 14, name: 'Tomato Man' },
     { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
+    { id: 16, name: 'Rubber Boy' },
     { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
+    { id: 18, name: 'Dr. IQ' },
     { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 20, name: 'Dorito' }
 ];
 var AppComponent = (function () {
     function AppComponent() {
--- ng/angular2-tour-of-heroes/app/app.component.ts
+++ ng/angular2-tour-of-heroes/app/app.component.ts
@@ -4,16 +4,16 @@ import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice'  },
-  { id: 12, name: 'Narco'     },
-  { id: 13, name: 'Bombasto'  },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta'   },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama'    },
-  { id: 18, name: 'Dr IQ'     },
-  { id: 19, name: 'Magma'     },
-  { id: 20, name: 'Tornado'   }
+  { id: 11, name: 'Mr. Mean'   },
+  { id: 12, name: 'Parco'      },
+  { id: 13, name: 'Bombasto'   },
+  { id: 14, name: 'Tomato Man' },
+  { id: 15, name: 'Magneta'    },
+  { id: 16, name: 'Rubber Boy' },
+  { id: 17, name: 'Dynama'     },
+  { id: 18, name: 'Dr. IQ'     },
+  { id: 19, name: 'Magma'      },
+  { id: 20, name: 'Dorito'     }
 ];
 
 @Component({
